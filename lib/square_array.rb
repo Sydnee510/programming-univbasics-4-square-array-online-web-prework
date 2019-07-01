2bd2242b69c9275ae953537c34b1cefc78688e56
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-07-01 19:51:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,3 +6,13 @@ while array[counter] do
    counter += 1
   end
 end
+
+def square_array(array)
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
+end
+
+my_arr = [1,2,3]
+p square_array(my_arr)
+end
